0e90af0eddd4faa13b2db26faf4f08510dd895e9
Tyler Potts <tylerpottsdev@gmail.com>
2021-01-07 01:34:43 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,141 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="app">
+		<header>
+			<h1>The<strong>Anime</strong>Database</h1>
+
+			<form class="search-box" @submit.prevent="HandleSearch">
+				<input 
+					type="search" 
+					class="search-field" 
+					placeholder="Search for an anime..."
+					required
+					v-model="search_query" />
+			</form>
+		</header>
+		<main>
+			<div class="cards" v-if="animelist.length > 0">
+				<Card 
+					v-for="anime in animelist" 
+					:key="anime.mal_id"
+					:anime="anime" />
+			</div>
+			<div class="no-results" v-else>
+				<h3>Sorry, we have no results...</h3>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Card from './components/Card';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	setup() {
+		const search_query = ref("");
+		const animelist = ref([]);
+
+		const HandleSearch = async () => {
+			animelist.value = await fetch(`https://api.jikan.moe/v3/search/anime?q=${search_query.value}`)
+				.then(res => res.json())
+				.then(data => data.results);
+
+			search_query.value = "";
+		}
+
+		return {
+			Card,
+			search_query,
+			animelist,
+			HandleSearch
+		}
+	}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+
+	font-family: 'Fira Sans', sans-serif;
+}
+
+a {
+	text-decoration: none;
+}
+
+header {
+	padding-top: 50px;
+	padding-bottom: 50px;
+
+	h1 {
+		color: #888;
+		font-size: 42px;
+		font-weight: 400;
+		text-align: center;
+		text-transform: uppercase;
+		margin-bottom: 30px;
+
+		strong {
+			color: #313131;
+		}
+
+		&:hover {
+			color: #313131;
+		}
+	}
+
+	.search-box {
+		display: flex;
+		justify-content: center;
+		padding-left: 30px;
+		padding-right: 30px;
+
+		.search-field {
+			appearance: none;
+			background: none;
+			border: none;
+			outline: none;
+
+			background-color: #F3F3F3;
+			box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+
+			display: block;
+			width: 100%;
+			max-width: 600px;
+			padding: 15px;
+			border-radius: 8px;
+
+			color: #313131;
+			font-size: 20px;
+
+			transition: 0.4s;
+
+			&::placeholder {
+				color: #AAA;
+			}
+
+			&:focus, &:valid {
+				color: #FFF;
+				background-color: #313131;
+				box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+			}
+		}
+	}
+}
+
+main {
+	max-width: 1200px;
+	margin: 0 auto;
+	padding-left: 30px;
+	padding-right: 30px;
+
+	.cards {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 -8px;
+	}
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="card">
+		<a :href="anime.url" target="_blank">
+			<img 
+				:src="anime.image_url" 
+				:alt="anime.title + ' Poster'" 
+			/>
+			<h3>{{ anime.title }}</h3>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['anime']
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+	flex: 1 1 33%;
+	width: 33%;
+	max-width: 33%;
+	padding: 0px 8px;
+	margin-bottom: 16px;
+
+	img {
+		width: 100%;
+		height: 300px;
+		object-fit: cover;
+		border-radius: 16px;
+		box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+		transition: 0.4s;
+	}
+
+	h3 {
+		padding: 16px;
+		color: #313131;
+		transition: .4s;
+	}
+
+	&:hover {
+		img {
+			transform: scale(1.05);
+		}
+
+		h3 {
+			color: #24C444;
+		}
+	}
+}
+</style>
